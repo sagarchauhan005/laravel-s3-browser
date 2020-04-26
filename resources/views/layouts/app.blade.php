08ffe7d307466271b0a19cabe2f868871e981a36
coolsagar005 <coolsagar005@gmail.com>
2020-04-26 15:41:09 +0530

    pagination count reduced

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,5 +12,15 @@
 <body>
 @include('layouts.nav')
 @yield('content')
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-41407532-2"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-41407532-2');
+</script>
+
 </body>
 </html>
